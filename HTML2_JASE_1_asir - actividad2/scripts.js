5335cca270f9fc6c815d9da919b03093abb22fe8
Ralfy2 <joseantonio.lacriptadedoge@gmail.com>
2025-01-14 13:35:18 +0100

    Add files via upload

---
+++ HTML2_JASE_1_asir - actividad2/scripts.js
@@ -0,0 +1,12 @@
+function escondinabo(id){
+    var elemento = document.getElementById(id);
+    if(!elemento) {
+        return true;
+    }
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+    } else {
+        elemento.style.display = "none"
+    };
+    return true;
+};
